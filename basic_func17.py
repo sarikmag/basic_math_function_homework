5cb53ac7b2bfc30238cdd24e6a0dd0337d6074fa
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-23 14:30:30 +0500

    asd

--- basic_func17.py
+++ basic_func17.py
@@ -1,7 +1,8 @@
 # Create a function called main.
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
+from math import pi
 def main(a):
-    a = 'pi'
+    a = pi
     return a
 print(main(1))
