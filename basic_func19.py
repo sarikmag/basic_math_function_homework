c83479924df01ea8dd48914d8ffb452021ff0ffe
sarikmag <sardorbek.abdujalilov@mail.ru>
2022-10-23 14:28:57 +0500

    asd

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,6 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+def main(a, b):
+    return abs(a-b)
+print(main(4, 11))
